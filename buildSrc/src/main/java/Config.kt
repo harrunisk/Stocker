bdd87f6017e7f7e6e398b2606ad4126dbd96e907
harrunisk <harrunisk@gmail.com>
2019-06-20 01:18:34 +0300

    Build src config

--- buildSrc/src/main/java/Config.kt
+++ buildSrc/src/main/java/Config.kt
@@ -15,22 +15,33 @@ object Config {
  * Auto generated buildConfig fileds
  */
 object Fields {
-
+    const val rootUrl = "ROOT_URL"
+    const val pName = "PACKAGE_NAME"
 }
 
 /*
  * Flavor Dimensions
  */
 object Dimensions {
+    const val default = "default"
 }
 
 /*
  * Product Flavors
  */
 object Prod {
+    const val versionCode = Config.versionCode
+    const val versionName = Config.versionName
+    const val packageName = Config.applicationId
 
 }
 
 object Dev {
+    const val suffix = ".dev"
+    const val versionCode = Config.versionCode * 10000
+    const val versionName = Config.versionName
+    const val versionNameSuffix = suffix
+    const val applicationIdSuffix = suffix
+    const val packageName = Config.applicationId + applicationIdSuffix
    }
 
