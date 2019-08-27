a2d134ff240472ea9e1b854109411377596057f2
harrunisk <harrunisk@gmail.com>
2019-08-27 23:26:09 +0300

    Dashboard modules were added

--- buildSrc/src/main/java/Modules.kt
+++ buildSrc/src/main/java/Modules.kt
@@ -4,6 +4,7 @@
 object Modules {
 
     private const val BASE_DIRECTORY = ":base"
+    private const val DASBOARD_DIRECTORY = ":dashboard"
 
     /*
      * App
@@ -18,4 +19,11 @@ object Modules {
     const val coreData = "$BASE_DIRECTORY:core_data"
     const val core = "$BASE_DIRECTORY:core"
     const val navigation = "$BASE_DIRECTORY:navigation"
+
+    /*
+    * Core Modules
+    */
+    const val dashboardPresentation = "$DASBOARD_DIRECTORY:dashboard_presentation"
+    const val dashboardDomain = "$DASBOARD_DIRECTORY:dashboard_domain"
+    const val dashboardData = "$DASBOARD_DIRECTORY:dashboard_data"
 }
