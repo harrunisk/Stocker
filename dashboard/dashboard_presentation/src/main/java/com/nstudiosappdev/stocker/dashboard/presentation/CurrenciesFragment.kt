bb20956a130a5012d0d5b1ed94635ace5076ec61
Harun <harrunisk@gmail.com>
2019-09-02 22:49:16 +0300

    Recycler view ui was designed

--- dashboard/dashboard_presentation/src/main/java/com/nstudiosappdev/stocker/dashboard/presentation/CurrenciesFragment.kt
+++ dashboard/dashboard_presentation/src/main/java/com/nstudiosappdev/stocker/dashboard/presentation/CurrenciesFragment.kt
@@ -1,12 +1,19 @@
 package com.nstudiosappdev.stocker.dashboard.presentation
 
 import android.os.Bundle
+import com.nstudiosappdev.core.model.DataHolder
 import com.nstudiosappdev.core.presentation.base.BaseViewModelFragment
+import com.nstudiosappdev.core.presentation.extensions.setup
 import com.nstudiosappdev.core.presentation.livedata.observeApi
+import com.nstudiosappdev.core.presentation.recyclerview.RecyclerViewAdapter
 import com.nstudiosappdev.stocker.presentation.R
 import kotlinx.android.synthetic.main.fragment_currencies.*
+import javax.inject.Inject
 
 class CurrenciesFragment : BaseViewModelFragment<CurrenciesViewModel>(){
+
+    @Inject
+    lateinit var currenciesAdapter: RecyclerViewAdapter
 
     override fun getModelClass() = CurrenciesViewModel::class.java
 
@@ -14,18 +21,32 @@ class CurrenciesFragment : BaseViewModelFragment<CurrenciesViewModel>(){
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-
-        viewModel.currencies.observeApi(this) {
-
-        }
-        viewModel.fetchCurrencies()
+        initObservers()
     }
 
     override fun initView() {
         super.initView()
-        get.setOnClickListener {
-            viewModel.fetchCurrencies()
+        initListeners()
+
+    }
+
+    private fun initListeners() {
+    }
+
+    private fun initObservers() {
+        viewModel.currencies.observeApi(this) {
+            when (it) {
+                is DataHolder.Success -> {
+                    currenciesAdapter.updateAllItems(it.data)
+                    currenciesRecyclerView.setup(
+                        adapter = currenciesAdapter,
+                        context = activity!!
+                    )
+                }
+
+            }
         }
+        viewModel.fetchCurrencies()
     }
 
     companion object {
--- dashboard/dashboard_presentation/src/main/java/com/nstudiosappdev/stocker/dashboard/presentation/CurrenciesViewModel.kt
+++ dashboard/dashboard_presentation/src/main/java/com/nstudiosappdev/stocker/dashboard/presentation/CurrenciesViewModel.kt
@@ -8,6 +8,8 @@ import com.nstudiosappdev.core.domain.Interactor
 import com.nstudiosappdev.core.error.ErrorFactory
 import com.nstudiosappdev.core.injection.modules.CoroutineManagerModule
 import com.nstudiosappdev.core.model.DataHolder
+import com.nstudiosappdev.core.presentation.recyclerview.DisplayItem
+import com.nstudiosappdev.core.presentation.recyclerview.DisplayItemListMapper
 import com.nstudiosappdev.core.presentation.viewmodel.BaseViewModel
 import com.nstudiosappdev.stocker.dashboard.domain.Currencies
 import com.nstudiosappdev.stocker.dashboard.domain.Currency
@@ -17,12 +19,13 @@ import javax.inject.Named
 class CurrenciesViewModel @Inject constructor(
     @Named(CoroutineManagerModule.CM_VIEWMODEL) coroutineManager: CoroutineManager,
     private val getCurrenciesInteractor: Interactor.DeferredRetrieveInteractor<List<Currency>>,
+    private val currenciesListMapper: DisplayItemListMapper<Currency>,
     private val errorFactory: ErrorFactory
 ) : BaseViewModel(coroutineManager) {
 
-    private val _currencies = MutableLiveData<DataHolder<List<Currency>>>()
+    private val _currencies = MutableLiveData<DataHolder<List<DisplayItem>>>()
 
-    val currencies: LiveData<DataHolder<List<Currency>>>
+    val currencies: LiveData<DataHolder<List<DisplayItem>>>
         get() = _currencies
 
     init {
@@ -31,7 +34,10 @@ class CurrenciesViewModel @Inject constructor(
 
     fun fetchCurrencies() = handleLaunch(execution = {
         _currencies.value = DataHolder.Loading()
-        _currencies.value = getCurrenciesInteractor.execute().await()
+        val currenciesResult = getCurrenciesInteractor.execute().await()
+        if(currenciesResult is DataHolder.Success) {
+            _currencies.value = DataHolder.Success(currenciesListMapper.map(currenciesResult.data))
+        }
     }, error = {
         _currencies.value = DataHolder.Fail(errorFactory.createErrorFromThrowable(it))
     })
