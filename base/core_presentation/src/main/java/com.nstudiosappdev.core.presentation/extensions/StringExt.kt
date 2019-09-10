58a97cd05e047c43c918efb525bfa7a4722e314c
Harun <harrunisk@gmail.com>
2019-09-10 23:47:10 +0300

    Currencies diff and transforms

---
+++ base/core_presentation/src/main/java/com.nstudiosappdev.core.presentation/extensions/StringExt.kt
@@ -0,0 +1,57 @@
+package com.nstudiosappdev.core.presentation.extensions
+
+import java.math.RoundingMode
+import java.net.MalformedURLException
+import java.net.URL
+import java.text.DecimalFormat
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.util.*
+
+const val BLANK = ""
+
+/*
+ * Checks if the given string is a valid url
+ */
+fun String?.isUrl(): Boolean {
+    return try {
+        URL(this)
+        true
+    } catch (e: MalformedURLException) {
+        false
+    }
+}
+
+fun String.dateInFormat(format: String): String? {
+    val date = Date(this.toLong())
+    val dateFormat = SimpleDateFormat(format, Locale.getDefault())
+    var time = BLANK
+    try {
+        time = dateFormat.format(date)
+    } catch (ignored: ParseException) {
+        ignored.printStackTrace()
+    }
+    return time
+}
+
+fun String?.capitalize(): String {
+    if (this == null || this.isEmpty()) {
+        return ""
+    }
+
+    val first = this[0]
+
+    return if (Character.isUpperCase(first)) {
+        this
+    } else {
+        Character.toUpperCase(first) + this.substring(1)
+    }
+}
+
+fun String.adjustSensitivityGiveString(sensitivity: Int): String {
+    return String.format("%.${sensitivity}f", this.replace(",", ".").toFloat())
+}
+
+fun String.adjustSensitivityGiveFloat(sensitivity: Int): Float {
+    return String.format("%.${sensitivity}f", this.replace(",", ".").toFloat()).toFloat()
+}
--- dashboard/dashboard_presentation/src/main/java/com/nstudiosappdev/stocker/dashboard/presentation/currencies/CurrenciesViewHolder.kt
+++ dashboard/dashboard_presentation/src/main/java/com/nstudiosappdev/stocker/dashboard/presentation/currencies/CurrenciesViewHolder.kt
@@ -5,23 +5,31 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.nstudiosappdev.core.presentation.extensions.adjustSensitivityGiveString
+import com.nstudiosappdev.core.presentation.extensions.adjustSensitivityGiveFloat
 import com.nstudiosappdev.core.presentation.recyclerview.DisplayItem
 import com.nstudiosappdev.core.presentation.recyclerview.ViewHolder
 import com.nstudiosappdev.core.presentation.recyclerview.ViewHolderBinder
 import com.nstudiosappdev.core.presentation.recyclerview.ViewHolderFactory
 import com.nstudiosappdev.stocker.presentation.R
 import javax.inject.Inject
+import kotlin.math.abs
 
 class CurrenciesViewHolder private constructor(itemView: View) : ViewHolder(itemView) {
 
     private val textViewBankName: TextView = itemView.findViewById(R.id.textViewBankName)
     private val textViewBuyingPrice: TextView = itemView.findViewById(R.id.textViewBuyingPrice)
     private val textViewSellingPrice: TextView = itemView.findViewById(R.id.textViewSellingPrice)
+    private val textViewDiff: TextView = itemView.findViewById(R.id.textViewDiff)
+
 
     private fun bind(item: CurrenciesViewEntity) {
         textViewBankName.text = item.bankName
-        textViewBuyingPrice.text = item.buyPrice
-        textViewSellingPrice.text = item.sellPrice
+        textViewBuyingPrice.text = item.sellPrice?.adjustSensitivityGiveString(4)
+        textViewSellingPrice.text = item.sellPrice?.adjustSensitivityGiveString(4)
+        textViewDiff.text = abs(
+                item.sellPrice!!.adjustSensitivityGiveFloat(4) - item.buyPrice!!.adjustSensitivityGiveFloat(4)
+                ).toString().adjustSensitivityGiveString(3)
 
         itemView.setOnClickListener {
             itemClickListener?.invoke(item)
